7a5f6b2e636b98e25b6cd5e10365ac5527b72811
dyturralbe <darylyturraldelopez@gmail.com>
2026-05-29 18:07:13 -0300

    [ADD] Actualizando Revision

--- od_subscription_draft_invoice/__manifest__.py
+++ od_subscription_draft_invoice/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Subscription Draft Invoices| Subscription Draft Invoice In Odoo 17 | Odoo 17 Subscription Draft Invoice',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0',
     'summary': 'Subscription Draft Invoice Odoo 17, Subscription Draft Invoices, Odoo 17 Subscription Draft Invoice',
     'description': 'Subscription Draft Invoice Odoo 17, Subscription Draft Invoices, Odoo 17 Subscription Draft Invoice',
     'sequence': '10',
@@ -9,7 +9,7 @@
     'live_test_url': 'https://youtu.be/dvd8DHcvHXU',
     'depends': ['sale_subscription'],
     'data': [
-        'views/sale_subscription_plan_views.xml'
+        # 'views/sale_subscription_plan_views.xml'
     ],
     'license': 'OPL-1',
     'price': 25,
